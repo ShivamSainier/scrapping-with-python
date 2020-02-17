b89fdffa46d9b6477b4ba7629558ee979eb8ce8f
shivam007-ss <ss1907364@gmail.com>
2020-02-18 00:16:17 +0530

    print articles

--- new.py
+++ new.py
@@ -3,8 +3,12 @@ import requests
 http='https://coreyms.com/'
 req=requests.get(http).text
 soup=bs.BeautifulSoup(req,'lxml')
-find=soup.find('iframe',class_="youtube-player")['src']
-l=find.split('/')[4]
-l=l.split('?')[0]
+#print all articles
+article=soup.find_all('article')
+print(article)
 
-print(l)
+
+#find=soup.find('iframe',class_="youtube-player")['src']
+#l=find.split('/')[4]
+#l=l.split('?')[0]
+#print(l)
